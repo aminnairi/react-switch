0d28c565ff1ba6175dcf327833db638877189dd8
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-05-29 18:34:03 +0200

    updated the example according to the recent default switch component addition

--- example/main.tsx
+++ example/main.tsx
@@ -1,27 +1,52 @@
-import React, { ChangeEventHandler, useState, useCallback, Fragment } from 'react'
-import { Switch, Case, Default } from "@aminnairi/react-switch";
+import React, { useEffect, useState } from 'react'
+import { Switch, Case, Default, DefaultSwitch } from "@aminnairi/react-switch";
+
+interface Article {
+  id: number;
+  title: string;
+  body: string;
+}
 
 export const Main = () => {
-  const [name, setName] = useState("Amin");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [articles, setArticles] = useState<Array<Article>>([]);
 
-  const updateName: ChangeEventHandler<HTMLInputElement> = useCallback((event) => {
-    setName(event.target.value);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts").then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return Promise.reject(new Error("Unable to fetch articles."));
+    }).then(json => {
+      setArticles(json);
+    }).catch((error: unknown) => {
+      setError(String(error));
+    }).finally(() => {
+      setLoading(false);
+    });
   }, []);
 
   return (
-    <Fragment>
-      <input type="text" value={name} onChange={updateName} />
-      <Switch>
-        <Case when={name === "Jake"}>
-          <h1>Hi Jake!</h1>
+    <Switch>
+      <Case when={loading}>
+        <p>Loading your articles, please wait...</p>
+      </Case>
+      <Case when={Boolean(error)}>
+        <p>Something went wrong: {error}</p>
+      </Case>
+      <DefaultSwitch>
+        <Case when={articles.length === 0}>
+          <p>There is no articles to display.</p>
         </Case>
-        <Case when={name === "Jane"}>
-          <h1>Hi Jane!</h1>
+        <Case when={articles.length === 1}>
+          <p>You have {articles.length} published article.</p>
         </Case>
         <Default>
-          <h1>Hi stranger!</h1>
+          <p>You have {articles.length} published articles.</p>
         </Default>
-      </Switch>
-    </Fragment>
+      </DefaultSwitch>
+    </Switch>
   );
 };
