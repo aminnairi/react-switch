e551e2fa2bd52f12184a20d4bad91c24b5a4838a
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-05-29 19:18:40 +0200

    added plugin to empty the build folder

--- sources/rollup.config.ts
+++ sources/rollup.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "rollup";
 import esbuild from "rollup-plugin-esbuild";
 import terser from "@rollup/plugin-terser";
+import remove from "rollup-plugin-delete";
 
 export default defineConfig({
   input: "index.tsx",
   plugins: [
+    remove({
+      targets: [
+        "build"
+      ]
+    }),
     esbuild(),
     terser(),
   ],
